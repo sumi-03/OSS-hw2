cb7f13cd18665f52baaf33efa9907e7e2bf694ff
sumi-03 <lsmkoo03@naver.com>
2023-11-16 13:35:17 +0000

    1-Commit

---
+++ Hw2Number1.java
@@ -0,0 +1,5 @@
+public class Hw2Number1{
+	public static void main(String[] args){
+		System.out.println("2291010 임수미");
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+// README.md
+This is the README file for the hw2-2291010-임수미 project.
